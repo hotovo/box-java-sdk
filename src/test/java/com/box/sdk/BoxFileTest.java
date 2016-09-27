a6ba0225d6a3a35ec020a6ccf8e5551414049198
gleb-urvanov <gleb.urvanov@hotovo.org>
2016-09-27 14:42:16 +0200

    get metadata template feature for files was tested

--- src/test/java/com/box/sdk/BoxFileTest.java
+++ src/test/java/com/box/sdk/BoxFileTest.java
@@ -16,6 +16,7 @@ import java.util.Collection;
 import java.util.Date;
 
 import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertThat;
 import static org.mockito.Matchers.anyLong;
@@ -26,10 +27,107 @@ import static org.mockito.Mockito.verify;
 
 import org.hamcrest.Matchers;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.experimental.categories.Category;
 
+import static com.github.tomakehurst.wiremock.client.WireMock.aResponse;
+import static com.github.tomakehurst.wiremock.client.WireMock.get;
+import static com.github.tomakehurst.wiremock.client.WireMock.stubFor;
+import static com.github.tomakehurst.wiremock.client.WireMock.urlMatching;
+
+import com.eclipsesource.json.JsonArray;
+import com.github.tomakehurst.wiremock.junit.WireMockRule;
+
 public class BoxFileTest {
+    @Rule
+    public final WireMockRule wireMockRule = new WireMockRule(8080);
+
+    @Test
+    @Category(UnitTest.class)
+    public void getMetadataTemplateSendsCorrectRequest() {
+        BoxAPIConnection api = new BoxAPIConnection("");
+        api.setBaseURL("http://localhost:8080/");
+        stubFor(get(urlMatching("/metadata_templates/global/properties/schema"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\"id\": \"0\"}")));
+
+        BoxFile.getMetadataTemplate(api);
+    }
+
+    @Test
+    @Category(UnitTest.class)
+    public void getMetadataTemplateParseAllFieldsCorrectly() {
+        final String templateKey = "productInfo";
+        final String scope = "enterprise_12345";
+        final String displayName = "Product Info";
+        final Boolean isHidden = false;
+        final JsonArray fields = JsonArray.readFrom("[\n"
+                        + "        {\n"
+                        + "            \"type\": \"float\",\n"
+                        + "            \"key\": \"skuNumber\",\n"
+                        + "            \"displayName\": \"SKU Number\",\n"
+                        + "            \"hidden\": false\n"
+                        + "        },\n"
+                        + "        {\n"
+                        + "            \"type\": \"enum\",\n"
+                        + "            \"key\": \"department\",\n"
+                        + "            \"displayName\": \"Department\",\n"
+                        + "            \"hidden\": false,\n"
+                        + "            \"options\": [\n"
+                        + "                {\n"
+                        + "                    \"key\": \"Beauty\"\n"
+                        + "                },\n"
+                        + "                {\n"
+                        + "                    \"key\": \"Accessories\"\n"
+                        + "                }\n"
+                        + "            ]\n"
+                        + "        }\n"
+                        + "    ]\n");
+
+        BoxAPIConnection api = new BoxAPIConnection("");
+        api.setBaseURL("http://localhost:8080/");
+        stubFor(get(urlMatching("/metadata_templates/global/properties/schema"))
+                .willReturn(aResponse()
+                        .withHeader("Content-Type", "application/json")
+                        .withBody("{\n"
+                                + "    \"templateKey\": \"productInfo\",\n"
+                                + "    \"scope\": \"enterprise_12345\",\n"
+                                + "    \"displayName\": \"Product Info\",\n"
+                                + "    \"hidden\": false,\n"
+                                + "    \"fields\": [\n"
+                                + "        {\n"
+                                + "            \"type\": \"float\",\n"
+                                + "            \"key\": \"skuNumber\",\n"
+                                + "            \"displayName\": \"SKU Number\",\n"
+                                + "            \"hidden\": false\n"
+                                + "        },\n"
+                                + "        {\n"
+                                + "            \"type\": \"enum\",\n"
+                                + "            \"key\": \"department\",\n"
+                                + "            \"displayName\": \"Department\",\n"
+                                + "            \"hidden\": false,\n"
+                                + "            \"options\": [\n"
+                                + "                {\n"
+                                + "                    \"key\": \"Beauty\"\n"
+                                + "                },\n"
+                                + "                {\n"
+                                + "                    \"key\": \"Accessories\"\n"
+                                + "                }\n"
+                                + "            ]\n"
+                                + "        }\n"
+                                + "    ]\n"
+                                + "}")));
+
+        MetadataTemplate template = BoxFile.getMetadataTemplate(api);
+        assertEquals(templateKey, template.getTemplateKey());
+        assertEquals(scope, template.getScope());
+        assertEquals(displayName, template.getDisplayName());
+        assertEquals(isHidden, template.isHidden());
+        assertEquals(fields, template.getFields());
+    }
+
     @Test
     @Category(IntegrationTest.class)
     public void uploadAndDownloadFileSucceeds() throws IOException {
@@ -449,7 +547,7 @@ public class BoxFileTest {
 
         Metadata check1 = uploadedFile.getMetadata();
         Assert.assertNotNull(check1);
-        Assert.assertEquals("bar", check1.get("/foo"));
+        assertEquals("bar", check1.get("/foo"));
 
         uploadedFile.delete();
     }
@@ -468,13 +566,13 @@ public class BoxFileTest {
 
         Metadata check1 = uploadedFile.getMetadata();
         Assert.assertNotNull(check1);
-        Assert.assertEquals("bar", check1.get("/foo"));
+        assertEquals("bar", check1.get("/foo"));
 
         uploadedFile.updateMetadata(check1.replace("/foo", "baz"));
 
         Metadata check2 = uploadedFile.getMetadata();
         Assert.assertNotNull(check2);
-        Assert.assertEquals("baz", check2.get("/foo"));
+        assertEquals("baz", check2.get("/foo"));
 
         uploadedFile.delete();
     }
